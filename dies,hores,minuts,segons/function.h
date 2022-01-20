c071ceda645d6d10ab443763fe18b46f1e4af2f1
EricAlbiol <95416862+EricAlbiol@users.noreply.github.com>
2022-01-20 12:20:03 +0100

    Add files via upload

---
+++ dies,hores,minuts,segons/function.h
@@ -0,0 +1,9 @@
+/*
+ * function.h
+ *
+ *  Created on: Jan 20, 2022
+ *      Author: admin-pg
+ */
+
+int introduir();
+void time(int segons);
---
+++ dies,hores,minuts,segons/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: Jan 20, 2022
+ *      Author: admin-pg
+ */
+#include <stdio.h>
+#include "function.h"
+void main(){
+	int segons;
+	segons = introduir();
+	time(segons);
+}
+
